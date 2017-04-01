5b9ea8b645a17609bd2e658f3703c107e4ee023e
mirza umer <mirzaumersaleem@yahoo.com>
2017-04-01 20:42:32 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+ import React, { Component } from 'react';
+//import newClass from './newClass';
+ import Demo from './Demo';
+//  import Table from './Table';
+//  import OrderList from './OrderList';
+//  import Un_Order_List from './un_Order_List';
+ class App extends Component {
+   render() {
+     return (
+       <div>
+       <Demo/>
+       {/*<OrderList/>*/}
+       {/*<Table/>*/}
+       {/*<Un_Order_List/>*/}
+       </div>
+
+     );
+   }
+ }
+ export default App;
---
+++ src/Demo.js
@@ -0,0 +1,114 @@
+import React,{Component} from 'react';
+class Demo extends Component
+{
+   constructor(){
+                super();
+                var temp=JSON.parse(localStorage.getItem("item"));
+                this.state={
+                arr : temp ==null? [] : temp,
+                inputText:'',
+                temp1:''
+                           }
+                }
+ databaseData(){
+
+      this.todo =  this.state.arr;
+      localStorage.setItem("item",JSON.stringify(this.todo));
+  }
+  /*retriveData(){
+      
+        //     this.todo = localStorage.getItem("item");
+        //     console.log("Before",this.todo);
+        // this.todo=JSON.parse(this.todo);
+        //         console.log("affer",this.todo);
+        //         for (var i=0 ;i<this.todo.length;i++){
+        //                console.log("\n",this.todo[i]);
+        //               }
+     this.state.temp1=localStorage.getItem("item");
+     this.state.temp1=JSON.parse(this.state.temp1);
+     this.setState({
+         arr:this.state.temp1
+     })
+      return(
+                this.state.arr.map((item, index) => {
+                return <li key={index} >
+                     {item}  <button onClick={this.deleteEntry.bind(this,index)}> delete</button>
+                     
+                </li>
+            })
+      )
+    }*/
+  getlist(){
+        return (
+            
+            this.state.arr.map((item, index) => {
+                return <li key={index} >
+                     {item}  <button onClick={this.deleteEntry.bind(this,index)}> delete</button>
+                     
+                </li>
+            }
+            )
+        )
+    }
+     setLocalStorage(){    
+         localStorage.setItem("item",JSON.stringify(this.state.arr))}
+  inputChange(changeValue){        
+            this.setState(
+			{inputText:changeValue.target.value});
+    }
+    addValue(event)
+    {
+    
+         this.state.arr.push(this.state.inputText);
+         this.setLocalStorage();
+        //databaseData();
+        this.setState({
+        arr:this.state.arr,
+      inputText:''
+     
+        })
+        event.preventDefault();
+        
+        // this.retriveData()
+    }
+    deleteEntry(index){
+       this.state.arr.splice(index,1);
+      this.setLocalStorage() ;
+        this.setState({
+        arr:this.state.arr
+
+  })
+
+    }
+ 
+  deleteall(){
+      this.state.arr.splice(0,5)
+      this.setLocalStorage()
+      
+this.setState({
+  arr:this.state.arr
+
+})
+
+  }
+
+    render(){
+    return(
+     <div>
+   
+     <ul>
+         <form onSubmit={this.addValue.bind(this)}>
+              <input type="text" value={this.state.inputText} onChange={this.inputChange.bind(this)}/>
+               <button onClick={this.addValue.bind(this)} >Add </button></form>
+
+         {this.getlist()}
+        
+     </ul>
+     <button onClick={this.deleteall.bind(this)}> Delete All</button>
+     
+     
+     </div>
+    );
+}
+}
+export default Demo;
